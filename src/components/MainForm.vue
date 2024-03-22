0d01ed53ba93baf906dc093e6cf6038f07f28a40
Daniel Disner <danidisner@gmail.com>
2024-03-22 10:21:25 -0300

    Primeira Funionalidade Marcador de Tempo

---
+++ src/components/MainForm.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Form for creating a new task">
+                <input class="input" type="text" placeholder="What needs to be done?" v-model="description">
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>{{ elapsedTime }}</strong>
+                    </section>
+                    <button class="button" @click="start">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>Play</span>
+                    </button>
+                    <button class="button" @click="stop">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>Stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'MainForm',
+    data() {
+        return {
+            timeInSeconds: 0,
+            stopwatch: 0,
+        }
+    },
+    computed: {
+        elapsedTime(): string {
+            return new Date(this.timeInSeconds * 1000).toISOString().substring(11, 19);
+        }
+    },
+    methods: {
+        start() {
+            this.stopwatch = setInterval(() => {
+                this.timeInSeconds += 1;
+            }, 1000)
+        },
+        stop() {
+            clearInterval(this.stopwatch);
+        }
+    }
+});
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="logo">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'SideBar',
+})
+</script>
+header
+<style scoped>
+header {
+    padding: 1rem;
+    background: #294057;
+    width: 100%;
+    height: 100vh;
+    padding: 32px
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
